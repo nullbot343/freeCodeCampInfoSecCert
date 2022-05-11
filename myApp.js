f4e4902e68538827b702253dca4e81bad36019ec
Ethan Marsh <99779610+nullbot343@users.noreply.github.com>
2022-05-11 12:55:32 +0000

    Ask Browsers to Access Your Site via HTTPS Only with helmet.hsts()

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
+const ninetyDaysInSeconds = 90*24*60*60;
 
 app.use(helmet());
 app.use(helmet.hidePoweredBy());
@@ -8,6 +9,10 @@ app.use(helmet.frameguard({action: "deny"}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({
+  maxAge: ninetyDaysInSeconds, 
+  force: true
+}));
 
 
 
